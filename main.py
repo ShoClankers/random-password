0876ce49c32d9465f3d5227602a8e6e33eb0d788
Thato <Nkadimeng.Thato@icloud.com>
2026-03-28 09:18:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_password(length=12):
+    
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    all_chars = letters + digits + symbols
+
+    password = ''.join(random.choice(all_chars) for _ in range(length))
+    
+    return password
+
+if __name__ == "__main__":
+    length = int(input("Enter password length: "))
+    print("Generated password:", generate_password(length))
